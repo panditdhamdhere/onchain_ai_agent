fe72aa6804d01dfde1ccd536f8b30e97b107d875
panditdhamdhere <dhamdherepandit@gmail.com>
2024-12-21 13:51:06 +0530

    finishes this off

--- src/openai/createThread.ts
+++ src/openai/createThread.ts
@@ -1,12 +1,15 @@
-import OpenAI from 'openai'
-import { Thread } from 'openai/resources/beta/threads/threads';
+import OpenAI from "openai";
+import { Thread } from "openai/resources/beta/threads/threads";
 
-export async function createThread(client: OpenAI, message: string): Promise<Thread> {
-    const thread = await client.beta.threads.create()
-    await client.beta.threads.messages.create(thread.id, {
-        role: "user",
-        content: message
-    })
+export async function createThread(client: OpenAI, message?: string): Promise<Thread> {
+    const thread = await client.beta.threads.create();
 
-    return thread
+    if (message) {
+        await client.beta.threads.messages.create(thread.id, {
+            role: "user",
+            content: message,
+        });
+    }
+
+    return thread;
 }
