4a6edba1ca7c443c315262258c51c85f8863f49e
panditdhamdhere <dhamdherepandit@gmail.com>
2024-12-21 11:11:23 +0530

    another fun thread added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import OpenAI from 'openai'
+import { createAssistant } from './openai/createAssistant';
+import { createThread } from './openai/createThread';
 async function main() {
 
     const client = new OpenAI();
+    const message = "Hello, Alt Cunningham!";
+
+    const assistant = await createAssistant(client);
+    const thread = await createThread(client, message)
     console.log("hello, world!")
 }
 
--- src/openai/createAssistant.ts
+++ src/openai/createAssistant.ts
@@ -4,6 +4,9 @@ import { Assistant } from 'openai/resources/beta/assistants'
 
 export async function createAssistant(client: OpenAI): Promise<Assistant> {
     return await client.beta.assistants.create({
-        model: "gpt-4o-mini"
+        model: "gpt-4o-mini",
+        name: "Alt cunningham",
+        instructions: `...`,
+        tools: [],
     });
 }
